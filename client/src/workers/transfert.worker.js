9fb83e4d28431d4b37a4de9b0b849d9b4013d80f
Mathieu Dugre <mathieu.dugre@mdugre.info>
2022-05-05 07:22:05 -0400

    Fix config idb download

--- client/src/workers/transfert.worker.js
+++ client/src/workers/transfert.worker.js
@@ -1,6 +1,10 @@
 import { expose } from 'comlink'
 import * as FiletransferDownloadClient from '@dugrema/millegrilles.reactjs/src/filetransferDownloadClient'
 import * as FiletransferUploadClient from '@dugrema/millegrilles.reactjs/src/filetransferUploadClient'
+
+// Configuration pour messagerie
+FiletransferDownloadClient.down_setNomIdb('messagerie')
+
 expose({
     ...FiletransferDownloadClient,
     ...FiletransferUploadClient
