1c829e4401f55ebffd4a3fa3a51f8c477381a2a2
Mathieu Dugre <mathieu.dugre@mdugre.info>
2022-05-05 19:18:41 -0400

    Cleanup regex validation

--- client/src/EditerContact.js
+++ client/src/EditerContact.js
@@ -5,7 +5,7 @@ import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import InputGroup from 'react-bootstrap/InputGroup'
 
-const VALIDATEUR_ADRESSE = /^(@[a-zA-Z][0-9a-zA-z_\.\-]*[a-zA-Z-0-9]?)\/((?:(?!\d+\.|-)[a-zA-Z0-9_\-]{1,63}(?<!-)\.?)+(?:[a-zA-Z]{2,}))$/
+const VALIDATEUR_ADRESSE = /^(@[a-zA-Z][0-9a-zA-z_.-]*[a-zA-Z-0-9]?)\/((?:(?!\d+\.|-)[a-zA-Z0-9_-]{1,63}(?<!-)\.?)+(?:[a-zA-Z]{2,}))$/
 
 console.debug("VALIDATEUR_ADRESSE : %O", VALIDATEUR_ADRESSE)
 
@@ -73,9 +73,7 @@ function EditerContact(props) {
         setTrusted(valeur)
     }, [setTrusted])
 
-    const retour = useCallback(()=>{
-        setUuidContactSelectionne('')
-    }, [setUuidContactSelectionne, setValidated])
+    const retour = useCallback(()=>setUuidContactSelectionne(''), [setUuidContactSelectionne])
     
     const sauvegarderCb = useCallback(event=>{
         event.preventDefault()
