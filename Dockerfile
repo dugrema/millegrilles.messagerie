f4d1283db583bfcd2effb6c0d98adfa60d21ebc0
Mathieu Dugre <mathieu.dugre@mdugre.info>
2022-07-06 09:18:44 -0400

    Update deps

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 # FROM node:16
-FROM docker.maceroc.com/millegrilles_webappbase:2022.2.1
+FROM docker.maceroc.com/millegrilles_webappbase:2022.4.0
 
 ENV MG_CONSIGNATION_HTTP=https://fichiers \
     APP_FOLDER=/usr/src/app \
--- docker-run.sh
+++ docker-run.sh
@@ -10,16 +10,16 @@ IMAGE_DOCKER=$REPO/${NAME}:${ARCH}_${VERSION}
 echo Image docker : $IMAGE_DOCKER
 
 # MQ
-export HOST=mg-dev5.maple.maceroc.com
-export HOST_MQ=mg-dev5
+export HOST=mg-dev1.maple.maceroc.com
+export HOST_MQ=mg-dev1
 
 CERT_FOLDER=/home/mathieu/mgdev/certs
 export MG_MQ_CAFILE=/certs/pki.millegrille.cert
 export MG_MQ_CERTFILE=/certs/pki.messagerie_web.cert
-export MG_MQ_KEYFILE=/certs/pki.messagerie_web.key
+export MG_MQ_KEYFILE=/certs/pki.messagerie_web.cle
 export MG_MQ_URL=amqps://$HOST_MQ:5673
-export MG_FICHIERS_URL=https://mg-dev5:3021
-export MG_REDIS_HOST=mg-dev5
+export MG_FICHIERS_URL=https://mg-dev1:3021
+export MG_REDIS_HOST=mg-dev1
 export PORT=3039
 
 export DEBUG=millegrilles:common:server5,millegrilles:messagerie:*
@@ -27,7 +27,7 @@ export DEBUG=millegrilles:common:server5,millegrilles:messagerie:*
 
 docker run --rm -it \
   --network host \
-  -v /home/mathieu/mgdev/certs:/certs \
+  -v /var/opt/millegrilles/secrets:/certs \
   -e MG_MQ_CAFILE -e MG_MQ_CERTFILE -e MG_MQ_KEYFILE \
   -e MG_MQ_URL -e HOST -e PORT \
   -e MG_FICHIERS_URL \
